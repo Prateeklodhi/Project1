8db7f62540d74ac5b550f2fd814521d9d39c7470
Prateek lodhi <41247754+Prateeklodhi@users.noreply.github.com>
2021-01-03 14:49:57 +0530

    Update settings.py

--- Project1/settings.py
+++ Project1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zif1i@-3o2t6ipcln5#o@*a9@=%(8%%t!5d&p9xmv-n97^*&7q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
